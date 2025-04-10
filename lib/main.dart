16a65c7afdd957aadd077f79fd156676a0ed0eef
ardhikakrsh <ardhikakrishna@gmail.com>
2025-04-11 01:38:22 +0700

    feat: add theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sqflite/service/database_service.dart';
 import 'package:flutter_sqflite/view/widget_tree.dart';
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 100, 1, 249),
+  brightness: Brightness.dark,
+);
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseService.instance.database;
@@ -10,14 +15,26 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
+        ),
+        scaffoldBackgroundColor: kDarkColorScheme.surface,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.surface,
+          foregroundColor: kDarkColorScheme.onSurface,
+        ),
+      ),
       title: 'Flutter Demo',
-      home: WidgetTree(),
+      home: const WidgetTree(),
     );
   }
 }
